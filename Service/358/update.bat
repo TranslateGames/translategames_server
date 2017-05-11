87f26d51049b759f80cae7eb062aa1e9921d2d86
Victor Alves <victoralves05@gmail.com>
2017-05-11 19:53:13 -0300

    Atualizando Traduções

--- Service/358/update.bat
+++ Service/358/update.bat
@@ -1,13 +1,13 @@
 @set caller=0
-@set version2="1000970"
-@set version3="1.0.0.0970"
+@set version2="1000971"
+@set version3="1.0.0.0971"
 @set sversion2c=1500277
 @set tam7z=200192
 CLS
 if %code%==358 (
 @set translationof="Dawn of War II e Chaos Rising"
 @set tam="2,83"
-@set totaltam=2976422
+@set totaltam=2976397
 @set installedsize="27,74"
 @set file=DOW2BR.7z
 @set changelog=- Tradução: Muitas Mudanças."^&Chr(13)^&"- Atualizador: Melhorias gerais de estabilidade, Imagens substituídas por códigos Base64 reduzidos, design geral melhorado, agora compatível com a API do Internet Explorer 9, Corrigido: Problemas de interface com o Windows XP, Adicionado: Verificação de Servidor Atual e melhorias na velocidade de carregamento."^&Chr(13)^&"Instalador: Melhorias gerais de segurança e estabilidade, Novo método de instalação em VBS, melhorias na velocidade de instalação e correções gerais."^&Chr(13)^&"Servidor: Adicionado Servidor GitHub e Removido Servidor DropBox.
