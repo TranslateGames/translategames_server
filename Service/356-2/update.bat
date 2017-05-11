b41e19016050ecf6a2f545f60080f711eb93ab17
Victor Alves <victoralves05@gmail.com>
2017-05-11 19:32:30 -0300

    Atualizando Traduções

--- Service/356-2/update.bat
+++ Service/356-2/update.bat
@@ -1,13 +1,13 @@
 @set caller=0
-@set version2="1002663"
-@set version3="1.0.0.2663"
+@set version2="1002664"
+@set version3="1.0.0.2664"
 @set sversion2c=1500277
 @set tam7z=200192
 CLS
 if %code%==356-2 (
 @set translationof="Age of Mythology: The Titans Expansion"
 @set tam="2,54"
-@set totaltam=2671212
+@set totaltam=2671194
 @set installedsize="4,28"
 @set file=XAOMBR.7z
 @set changelog=- Tradução: Algumas Mudanças."^&Chr(13)^&"- Atualizador: Melhorias gerais de estabilidade, Imagens substituídas por códigos Base64 reduzidos, design geral melhorado, agora compatível com a API do Internet Explorer 9, Corrigido: Problemas de interface com o Windows XP, Adicionado: Verificação de Servidor Atual e melhorias na velocidade de carregamento."^&Chr(13)^&"Instalador: Melhorias gerais de segurança e estabilidade, Novo método de instalação em VBS, melhorias na velocidade de instalação e correções gerais."^&Chr(13)^&"Servidor: Adicionado Servidor GitHub e Removido Servidor DropBox.
