af95d6b314125dc7473171786d4561cbe1dc7dcf
Victor Alves <victoralves05@gmail.com>
2019-06-23 01:32:34 -0300

    Teste de Mudanças 8

--- Service/350-4/update.bat
+++ Service/350-4/update.bat
@@ -9,7 +9,7 @@ if %code%==350-4 (
 @set tam="3,49"
 @set totaltam=3668745
 @set installedsize="61,18"
-@set hash=125E11E527E1C6FE72359CCBB994BD1F519A5249E1C53F682689D4DA98E7A3C8
+@set hash=57B597C45E45EBA9ADB9121BC8B43D6C915A28A4C78F85B70290603E7D757EA1
 @set file=SSBR.7z
 @set changelog=- Tradução: Algumas Mudanças.\n - Atualizador: Melhorias gerais de estabilidade, Melhorias de segurança em todas as conexões e Desempenho aprimorado.\n - Interface: Melhorias Gerais de estabilidade e Correções de erros.\n - Instalador\Atualizador: Melhorias gerais de segurança e estabilidade.\n - Servidor: Melhorias gerais e Adicionado: Suporte completo ao protocolo HTTPS.
 )
