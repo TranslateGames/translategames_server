84a1019627bffecc046abb3245eaab45b7ce9fb9
Victor Alves <victoralves05@gmail.com>
2019-06-24 19:41:44 -0300

    Teste de Mudanças 30

--- Service/350-4/update.bat
+++ Service/350-4/update.bat
@@ -8,9 +8,9 @@ CLS
 if %code%==350-4 (
 @set translationof="Dawn of War - Soulstorm"
 @set tam="3,49"
-@set totaltam=3668747
+@set totaltam=3668745
 @set installedsize="61,18"
-@set hash=8D5BEEDA7501B785784498AC192E842A8BA5D99C4B9C5317C505694A7E751DC3
+@set hash=2F12C431A5D72E4D3D3777C08E669CB893F49172CDC1C076BAAEA387D02CE29A
 @set file=SSBR.7z
 @set changelog=- Tradução: Algumas Mudanças.\n - Atualizador: Melhorias gerais de estabilidade, Melhorias de segurança em todas as conexões e Desempenho aprimorado.\n - Interface: Melhorias Gerais de estabilidade e Correções de erros.\n - Instalador\Atualizador: Melhorias gerais de segurança e estabilidade.\n - Servidor: Melhorias gerais e Adicionado: Suporte completo ao protocolo HTTPS.
 )
