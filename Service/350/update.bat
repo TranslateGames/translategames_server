9fe1c38257009f4e91790df167f5c6fe102c7e58
Victor Alves <victoralves05@gmail.com>
2020-06-18 03:05:28 -0300

    Teste de Mudanças

--- Service/350/update.bat
+++ Service/350/update.bat
@@ -8,9 +8,9 @@ CLS
 if %code%==350 (
 @set translationof="Dawn of War e Winter Assault"
 @set tam="3,81"
-@set totaltam=4005460
+@set totaltam=4005458
 @set installedsize="20,35"
-@set hash=E2CB413EC2C43EDE0015D90A436957E7BA5ABF9D961BA951CCD9932CDC2B5C53
+@set hash=FB408BE875D91D6633FFEA98DBB59F35CFB7BEC30EEC592D7F7BCE91044A809A
 @set file=W4BR.7z
 @set changelog=- Tradução: Algumas mudanças.\n - Atualizador: Desempenho aprimorado, Correção Urgente: Corrigido uma falha que faz com que o atualizador automático não funcione corretamente quando instalado pela primeira vez.\n - Interface: Correções de erros e Mudanças na interface de configurações.\n - Instalador\Atualizador: Melhorias gerais.\n - Servidor: Melhorias gerais.
 )
